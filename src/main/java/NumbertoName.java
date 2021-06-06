9ba05258573663053c7086c6712140ad2c25f90d
tanushka <tanushkakommoju@knights.ucf.edu>
2021-06-05 22:05:32 -0400

    ex 21

---
+++ src/main/java/NumbertoName.java
@@ -0,0 +1,56 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Tanushka Kommoju
+ */
+
+import java.util.Scanner;
+
+public class NumbertoName {
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of the month: ");
+        int i = input.nextInt();
+        String month = "";
+        switch(i){
+            case 1:
+                month = "January";
+                break;
+            case 2:
+                month = "February";
+                break;
+            case 3:
+                month = "March";
+                break;
+            case 4:
+                month = "April";
+                break;
+            case 5:
+                month = "May";
+                break;
+            case 6:
+                month = "June";
+                break;
+            case 7:
+                month = "July";
+                break;
+            case 8:
+                month = "August";
+                break;
+            case 9:
+                month = "September";
+                break;
+            case 10:
+                month = "October";
+                break;
+            case 11:
+                month = "November";
+                break;
+            case 12:
+                month = "December";
+                break;
+        }
+        System.out.println("The name of the month is "+month);
+    }
+}
+
+
